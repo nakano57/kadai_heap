68e7a9c621b0ddd7cf643214617b7285b993b5f1
nakano57 <hiroto856@gmail.com>
2019-05-21 17:36:56 +0900

    kansei

--- loop.py
+++ loop.py
@@ -5,9 +5,9 @@ import time
 if __name__ == "__main__":
     #start = time.time()
 
-    print("n,x,time")
+    print("n,time")
 
-    for i in range(1, 1000):
+    for i in range(1, 10000):
         start = time.time()
         res = subprocess.run(["./main", str(i)], stdout=subprocess.PIPE)
         # elapsed_time = time.time() - start
